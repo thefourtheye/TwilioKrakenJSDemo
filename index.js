6d551d8c459094e6f96e86fc5738b049d119348d
thefourtheye <thechargingvolcano@gmail.com>
2014-05-27 19:29:58 +0530

    Included basic DB Initialization setup

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 'use strict';
 
-
 var kraken = require('kraken-js'),
     app = {},
-    db = require("sqlite3");
+    db = require("./lib/db/DBManager.js");
 
 
 app.configure = function configure(nconf, next) {
     // Async method run on startup.
+    db.initialize(nconf.get("database"));
     next(null);
 };
 
 
 app.requestStart = function requestStart(server) {
-    var sqlite3 = require('sqlite3').verbose();
-    var db = new sqlite3.Database('Database.sqlite3');
+    // var sqlite3 = require('sqlite3').verbose();
+    // var db = new sqlite3.Database('Database.sqlite3');
 
-    db.serialize(function() {
-        // db.run("DROP TABLE IF EXISTS lorem");
-        // db.run("CREATE TABLE lorem (info TEXT)");
-        db.run("CREATE TABLE IF NOT EXISTS lorem (info TEXT)");
+    // db.serialize(function() {
+    //     // db.run("DROP TABLE IF EXISTS lorem");
+    //     // db.run("CREATE TABLE lorem (info TEXT)");
+    //     db.run("CREATE TABLE IF NOT EXISTS lorem (info TEXT)");
 
-        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-        for (var i = 0; i < 3; i++) {
-            stmt.run("Ipsum " + i);
-        }
-        stmt.finalize();
+    //     var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    //     for (var i = 0; i < 3; i++) {
+    //         stmt.run("Ipsum " + i);
+    //     }
+    //     stmt.finalize();
 
-        db.all("SELECT rowid AS id, info FROM lorem", function(err, rows) {
-            console.log(rows);
-        });
-    });
+    //     db.all("SELECT rowid AS id, info FROM lorem", function(err, rows) {
+    //         console.log(rows);
+    //     });
+    // });
 
-    db.close();
+    // db.close();
 };
 
 
