c76e72833f949dca7b17cdefbbbce3c327baa8ea
thefourtheye <thechargingvolcano@gmail.com>
2014-05-28 15:08:30 +0530

    Created the basic models

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var kraken = require('kraken-js'),
     dbManager = require("./lib/db/dbmanager.js"),
     db;
 
-
 app.configure = function configure(nconf, next) {
     // Async method run on startup.
     dbManager.initialize(nconf.get("database"));
@@ -14,21 +13,17 @@ app.configure = function configure(nconf, next) {
     next(null);
 };
 
-
 app.requestStart = function requestStart(server) {
 
 };
-
 
 app.requestBeforeRoute = function requestBeforeRoute(server) {
     // Run before any routes have been added.
 };
 
-
 app.requestAfterRoute = function requestAfterRoute(server) {
     // Run after all routes have been added.
 };
-
 
 if (require.main === module) {
     kraken.create(app).listen(function(err) {
---
+++ models/SpeedDial.js
@@ -0,0 +1,26 @@
+var db = require("./lib/db/dbmanager.js").getDBObject(),
+    _ = require("lodash");
+
+function Contact(obj) {
+    var database = "",
+        table = "SpeedDial";
+
+    return {
+        "select": function(callBack) {
+            db.fetch(database, table, _.keys(obj), _.values(obj), callBack);
+        },
+        "insert": function() {
+            db.insert(database, table, obj);
+        },
+        "purge": function() {
+            db.purge(database, table, obj);
+        },
+        "update": function(criteria) {
+            db.update(database, table, obj, criteria);
+        },
+        "query": db.query,
+        "selectQuery": db.selectQuery
+    }
+}
+
+module.exports = Contact;
