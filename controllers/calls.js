539365e4c1c645331a59e12610df18c54304a1b9
thefourtheye <thechargingvolcano@gmail.com>
2014-05-29 10:56:48 +0530

    Included the basic calling version

---
+++ controllers/calls.js
@@ -0,0 +1,31 @@
+'use strict';
+var _ = require("lodash"),
+    CommonError = require("../lib/util/commonerror"),
+    logger = require("../lib/util/logger"),
+    nconf = require("nconf"),
+    accountSid = nconf.get("SID"),
+    authToken = nconf.get("SToken"),
+    myNumber = nconf.get("MyNumber");
+
+module.exports = function(app) {
+    app.post('/call', function(req, res) {
+        console.log(req.body, accountSid, authToken);
+        var client = require('twilio')(accountSid, authToken);
+        client.calls.create({
+            to: req.body.number,
+            from: myNumber,
+            method: "GET",
+            fallbackMethod: "GET",
+            statusCallbackMethod: "GET",
+            record: "false",
+            url: "http://twilio-krakenjs-demo.herokuapp.com/acceptedCall"
+        });
+        res.send(200);
+    });
+
+    app.get('/acceptedCall', function(req, res) {
+        resset('Content-Type', 'text/xml');
+        res.send('<?xml version="1.0" encoding="UTF-8" ?><Response>' +
+            "<Say>Hello from twilio-krakenjs-demo application</Say></Response>");
+    });
+}
